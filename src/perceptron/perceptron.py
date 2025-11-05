844b739654ff211f3e6ba706d230660bae904a02
AndrewwBC <andrew-hot2011@hotmail.com>
2025-11-05 01:53:34 -0300

    perceptron: fix fit method

--- src/perceptron/perceptron.py
+++ src/perceptron/perceptron.py
@@ -1,31 +1,32 @@
 import numpy as np
+from sklearn import datasets
+import pandas as pd
+from sklearn.metrics import accuracy_score
 
 class Perceptron:
     
-    def __init__(self, data, n_iter, target): 
-        rgen = np.random.RandomState(42)
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
         self.n_iter = n_iter
-        self.target = target
-        self.data = data
-        self.features = self.data.drop(columns = self.target).values
-        self.weights = rgen.normal(loc=0.0, scale=0.01, size=data.drop(columns = target).shape[1])
-        self.bias = rgen.random()
+        self.random_state = random_state
     
-    def network_input(self, xi):
-        return np.dot(xi, self.weights) + self.bias
+    def network_input(self, feature):
+        return np.dot(self.weights, feature) + self.bias
     
-    def fit(self):
+    def fit(self, features, targets):
         eta = 0.1
-        target_values = self.data[self.target].values
+        rgen = np.random.RandomState(42)
+        self.weights = rgen.normal(loc=0.0, scale=0.01, size=features.shape[1])
+        self.bias = rgen.random()
         
         for _ in range(self.n_iter):
             
-            for xi, target in zip(self.features, target_values):
+            for xi, target in zip(features, targets):
                 predicted = self.predict(xi)
                 update = eta * (target - predicted)
                 self.weights += update *  xi
                 self.bias += update
-    
+        return self
     def predict(self, x):
         z = self.network_input(x)
         return np.where(z >= 0.0, 1, 0)
