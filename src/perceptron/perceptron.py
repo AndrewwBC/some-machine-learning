6a83b8794a755c88d43f19cd8888c420afc1d4b6
AndrewwBC <andrew-hot2011@hotmail.com>
2025-11-05 01:28:08 -0300

    perceptron: add network_input method

--- src/perceptron/perceptron.py
+++ src/perceptron/perceptron.py
@@ -1,5 +1,4 @@
 import numpy as np
-from sklearn import datasets
 
 class Perceptron:
     
@@ -11,36 +10,7 @@ class Perceptron:
         self.features = self.data.drop(columns = self.target).values
         self.weights = rgen.normal(loc=0.0, scale=0.01, size=data.drop(columns = target).shape[1])
         self.bias = rgen.random()
-        
-        
+    
     def network_input(self, xi):
         return np.dot(xi, self.weights) + self.bias
     
-    def fit(self):
-        eta = 0.1
-        target_values = self.data[self.target].values
-        
-        for _ in range(self.n_iter):
-            
-            for xi, target in zip(self.features, target_values):
-                predicted = self.predict(xi)
-                update = eta * (target - predicted)
-                print(f"Previu:{predicted} - Alvo: {target}")
-                self.weights += update *  xi
-                self.bias += update
-    
-    def predict(self, x):
-        z = self.network_input(x)
-        return np.where(z >= 0.0, 1, 0)
-  
-
-
-iris = datasets.load_iris(as_frame=True)
-data = iris.frame[iris.feature_names[:2] + ['target']]
-data = data[data['target'] != 2]  # binário 0/1
-
-p = Perceptron(data, n_iter=10, target='target')
-p.fit()
-
-# Predição
-print(p.predict(data.drop(columns='target').values[3]))
