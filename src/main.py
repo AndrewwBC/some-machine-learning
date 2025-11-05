79c248ddb95a985b6f6ce8a848ef09e1265b0424
AndrewwBC <andrew-hot2011@hotmail.com>
2025-11-05 11:21:38 -0300

    perceptron: add predict method to return list

---
+++ src/main.py
@@ -0,0 +1,28 @@
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from perceptron import file
+
+iris = datasets.load_iris()
+
+features = iris.data[:, [2, 3]][0:100]
+target = iris.target[0:100]
+
+features_train, features_test, target_train, target_test = train_test_split(
+    features, target, test_size=0.3, random_state=1, stratify=target
+)
+
+scaler = StandardScaler()
+scaler.fit(features_train)
+features_train_std = scaler.transform(features_train)
+features_test_std = scaler.transform(features_test)
+
+perceptron = file.Perceptron()
+perceptron.fit(features_train_std, target_train)
+
+predicted = perceptron.predict(features_test_std)
+acc = accuracy_score(target_test, predicted)
+print(acc)
--- src/perceptron/file.py
+++ src/perceptron/file.py
@@ -8,24 +8,35 @@ class Perceptron:
         self.random_state = random_state
     
     def network_input(self, feature):
-        return np.dot(self.weights, feature) + self.bias
+        return np.dot(feature, self.weights) + self.bias
     
     def fit(self, features, targets):
         rgen = np.random.RandomState(self.random_state)
         self.weights = rgen.normal(loc=0.0, scale=0.01, size=features.shape[1])
-        self.bias = rgen.random()
+        self.bias = np.float64(0.)
         
         for _ in range(self.n_iter):
             self.predictions = []
+            
             for xi, target in zip(features, targets):
-                predicted = self.predict(xi)
+                predicted = self.predicted(xi)
                 update = self.eta * (target - predicted)
+                
                 self.predictions.append(predicted)
                 self.weights += update *  xi
                 self.bias += update
        
         return self
     
-    def predict(self, x):
+    def predicted(self, x):
         z = self.network_input(x)
+        print(z)
         return np.where(z >= 0.0, 1, 0)
+    
+    def predict(self, x):
+        predict_list = []
+        for row in x:
+            z = self.network_input(row)
+            predict_list.append(np.where(z >= 0.0, 1, 0))
+        return predict_list
+        
